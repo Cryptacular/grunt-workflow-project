5c88a35682d13296cc1f376eb9baf6c4ece05200
Nick Mertens <nick.mertens@image-centre.com>
2015-03-11 16:08:09 +1300

    Project set up

---
+++ gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    connect: {
+			uses_defaults: {}
+		},
+		sass: {
+      dev: { 
+        options: { sourceMap: true },
+        files: { 'assets/dev/css/style.css' : 'assets/dev/scss/style.scss' }
+      },
+			prod: { 
+        options: { 
+					sourceMap: false,
+					outputStyle: 'compressed'
+				},
+        files: { 'assets/prod/css/style.css' : 'assets/dev/scss/style.scss' }
+      }
+    },
+		watch: {
+			css: {
+				files: 'assets/dev/scss/**/*.scss',
+				tasks: [ 'sass:dev' ],
+				options: { livereload: true }
+			},
+			html: {
+				files: '*.html',
+				options: { livereload: true }
+			}
+		}
+  });
+  // Load Grunt plugins
+  grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+  // Default task(s).
+  grunt.registerTask('default', []);
+};
